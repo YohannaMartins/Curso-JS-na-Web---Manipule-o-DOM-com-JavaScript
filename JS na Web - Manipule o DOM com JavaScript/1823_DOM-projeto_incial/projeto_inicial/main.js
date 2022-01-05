5084a1a71e39e31117af91453841844ab0ee367b
Yohanna Martins <91335910+YohannaMartins@users.noreply.github.com>
2022-01-05 17:10:07 -0300

    Add files via upload

---
+++ JS na Web - Manipule o DOM com JavaScript/1823_DOM-projeto_incial/projeto_inicial/main.js
@@ -0,0 +1,27 @@
+import BotaoConclui from "./assets/componentes/concluiTarefa";
+import BotaoDeleta from "./assets/componentes/deletaTarefa";
+    
+    const criarTarefa = (evento) => {
+
+    evento.preventDefault();
+
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);
+    input.value = " ";
+    }
+
+    const novaTarefa = document.querySelector('[data-form-button]');
+
+
+    novaTarefa.addEventListener('click', criarTarefa);
